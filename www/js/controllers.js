ec5bae0d3c7c6f3053269e6e2052b8ceb02413ab
Marek Kalnik <marekk@theodo.fr>
2015-05-14 13:23:02 +0200

    Add call to estimotes in controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,16 @@
 angular.module('starter.controllers', [])
 
-  .controller('StartCtrl', function($scope) {
-    console.log('hello');
+  .controller('StartCtrl', function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      estimote.beacons.startEstimoteBeaconsDiscoveryForRegion(
+        {}, // Empty region matches all beacons.
+        function (beaconInfo) {
+          angular.forEach(beaconInfo.beacons, function (beacon) {
+            console.log(beacon);
+          })
+        },
+        function (err) {
+          console.log(err);
+        });
+    });
   });
